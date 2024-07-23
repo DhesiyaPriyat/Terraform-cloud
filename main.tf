1fe475c0137fa30cb2d6d6ace51d4d39b32de958
root <root@ip-172-31-4-92.ap-south-1.compute.internal>
2024-07-23 09:51:48 +0000

    Code

---
+++ main.tf
@@ -0,0 +1,88 @@
+terraform {
+  required_providers {
+    aws = {
+      source  = "hashicorp/aws"
+      version = "~> 5.0"
+    }
+  }
+}
+# Configure the AWS Provider
+provider "aws" {
+  region = "us-east-1"
+}
+resource "aws_vpc" "my_vpc" {
+  cidr_block       = "10.0.0.0/16"
+  instance_tenancy = "default"
+
+  tags = {
+    Name = "VPC-1"
+  }
+}
+resource "aws_subnet" "pubsub" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.1.0/24"
+  availability_zone = "us-east-1a"
+  tags = {
+    Name = "PUBLIC_SUB"
+  }
+}
+resource "aws_subnet" "privsub" {
+  vpc_id     = aws_vpc.my_vpc.id
+  cidr_block = "10.0.2.0/24"
+  availability_zone = "us-east-1b"
+  tags = {
+    Name = "PRIVATE_SUB"
+  }
+}
+resource "aws_internet_gateway" "igw" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  tags = {
+    Name = "INTERNET_GATEWAY"
+  }
+}
+resource "aws_route_table" "pubrt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_internet_gateway.igw.id
+  }
+
+  tags = {
+    Name = "PUBLIC_RT"
+  }
+}
+resource "aws_route_table_association" "pubrtsubassoc" {
+  subnet_id      = aws_subnet.pubsub.id
+  route_table_id = aws_route_table.pubrt.id
+}
+resource "aws_eip" "myeip" {
+}
+resource "aws_nat_gateway" "natgw" {
+  allocation_id = aws_eip.myeip.id
+  subnet_id     = aws_subnet.pubsub.id
+
+  tags = {
+    Name = "NATGW"
+  }
+
+  # To ensure proper ordering, it is recommended to add an explicit dependency
+  # on the Internet Gateway for the VPC.
+}
+resource "aws_route_table" "privrt" {
+  vpc_id = aws_vpc.my_vpc.id
+
+  route {
+    cidr_block = "0.0.0.0/0"
+    gateway_id = aws_nat_gateway.natgw.id
+  }
+
+  tags = {
+    Name = "PRIVATE_RT"
+  }
+}
+resource "aws_route_table_association" "privrtsubassoc" {
+  subnet_id      = aws_subnet.privsub.id
+  route_table_id = aws_route_table.privrt.id
+}
